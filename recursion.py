43c7f721368ce5a391d0a9f20251498ebc90eb93
Hau Chong <thchong-code@users.noreply.github.com>
2020-11-17 12:41:33 +0800

    recursion 2

--- recursion.py
+++ recursion.py
@@ -146,4 +146,12 @@ print(result)
 # Counting partitions
 
 
-def count_partitations(n, m):
+def count_partitions(n, m):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        return count_partitions(n - m, m) + count_partitions(n, m - 1)
