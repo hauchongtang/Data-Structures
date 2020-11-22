d6ff9dc959550fd5e2eafcd8342918237361403d
hauchongtang <hauchongtang@users.noreply.github.com>
2020-11-22 21:50:19 +0800

    added trees.py file

---
+++ trees.py
@@ -0,0 +1,7 @@
+"""
+ The tree is a fundamental data abstraction that imposes regularity 
+ on how hierarchical values are structured and manipulated.
+ A tree has a root label and a sequence of branches. Each branch of a tree is a tree.
+ A tree with no branches is called a root.
+ 
+"""
